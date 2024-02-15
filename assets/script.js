d2e1a15ae373f52cd864190cee37013c50343932
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-02-15 12:57:47 +0000

    result displayed in html

--- assets/script.js
+++ assets/script.js
@@ -13,15 +13,38 @@ searchBtn.addEventListener("click",function(event){
 calculateLeapYear=(year)=>{
 //The logic for calculating Leap Year if year is divisible by 4 
 //exception is end of century is leap year when divisible by 400
-
+let result="";
     if(year%100!=0&&year%4==0){
-        console.log(`Yes, ${year} is a Leap Year!`);
+        result=`Yes, ${year} is a Leap Year!`;
         
     }
     else if(year%100===0&&year%400===0){
-        console.log(`Yes, ${year} is a century and a Leap Year!`);
+        result=`Yes, ${year} is a century and a Leap Year!`;
     }
 
-    else{ console.log(`No, the year is not a leap year.`);
+    else{ 
+        result=`No, the year is not a leap year.`;
         }
+
+    createDisplay(result,year);
+
+}
+
+createDisplay=(result,year)=>{
+    const resultDisplay=document.querySelector("#result");
+    const divCardTitle=document.createElement("div");
+    divCardTitle.setAttribute("class","title");
+    const yearTitle=document.createElement("h3");
+    yearTitle.textContent=year;
+    console.log(yearTitle);
+    divCardTitle.append(yearTitle);
+    const divCardResult=document.createElement("div");
+    const isLeapYear=document.createElement("p");
+    isLeapYear.textContent=result;
+    console.log(isLeapYear);
+    divCardResult.append(isLeapYear);
+    resultDisplay.appendChild(divCardTitle);
+    resultDisplay.appendChild(divCardResult);
+    console.log(resultDisplay);
+
 }
